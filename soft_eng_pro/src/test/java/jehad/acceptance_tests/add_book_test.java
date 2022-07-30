d7b17b7e74ba74f37d2726c7e501d2334be5bb07
Firas-Aghbar <108176323+Firas-Aghbar@users.noreply.github.com>
2022-07-31 02:57:27 +0300

    last commit firas

--- soft_eng_pro/src/test/java/jehad/acceptance_tests/add_book_test.java
+++ soft_eng_pro/src/test/java/jehad/acceptance_tests/add_book_test.java
@@ -2,58 +2,45 @@ package jehad.acceptance_tests;
 
 import static org.junit.Assert.*;
 
+import java.util.*;
+import java.util.Map;
+
+import jehad1.*;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 public class add_book_test {
 	
-	jehad1.admin_user admin_user;
-	jehad1.books book ;
-	public add_book_test() {
-		
-		 admin_user = new jehad1.admin_user();
-		book  =new jehad1.books() ;
-
-		
+	admin_user admin;
+	books book ;
+	ELibrary library;
+	public add_book_test() {		
+		admin = new admin_user();
+		book  =new books() ;
+		library = new ELibrary();
 	}
 	
 	
 	
 	@Given("that the administrator is logged in")
-	public void thatTheAdministratorIsLoggedIn() {
-		
-		admin_user.the_Login("adminadmin");
-
-		
+	public void thatTheAdministratorIsLoggedIn() {		
+		admin.login("adminadmin");		
 	}
 	
 	@Given("there is a book with title {string}, author {string}, and signature {string}")
 	public void thereIsABookWithTitleAuthorAndSignature(String string, String string2, String string3) {
-		book  = new jehad1.books() ;
-		 book.adding_book( "Head First Java",  "Ali Ahmad",  "Ali99");
-		
-		
+		book  = new books(string, string2, string3);
 	}
 	
 	@When("the book is added to the library")
 	public void theBookIsAddedToTheLibrary() {
-		book.is_Exist();
-	   
+		library.addBook(book);
 	}
 	@Then("the book with title {string}, author {string}, and signature {string} is contained in the library")
 	public void theBookWithTitleAuthorAndSignatureIsContainedInTheLibrary(String string, String string2, String string3) {
-		book  = new jehad1.books() ;
-		 book.adding_book( "Head First Java",  "Ali Ahmad",  "Ali99");
-		assertEquals(book.is_Exist() , false );
-		 book.search("Ali99");
-		assertEquals( book.is_Found() , true );
+		HashMap<books, books> tmp = new HashMap<books, books>();
+		tmp.put(book, book);
+		assertEquals(library.searchBook(string3),tmp);
 	}
-	@Given("that the administraator is not logged in")
-	public void thatTheAdministratoorIsNotLoggedIn() {
-		admin_user.logout();
-	}
-	@Then("the error messsage {string} is given")
-	public void theErrorMessaggeIsGiven(String string) {
-	    System.out.println("Administrator login required");
-	}
+	
 } 
