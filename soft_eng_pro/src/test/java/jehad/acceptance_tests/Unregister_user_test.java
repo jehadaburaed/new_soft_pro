19a7724dab4a75f27bedc8b23cbf7c0825b6f117
Firas-Aghbar <108176323+Firas-Aghbar@users.noreply.github.com>
2022-07-31 09:56:52 +0300

    last commit firas 2

--- soft_eng_pro/src/test/java/jehad/acceptance_tests/Unregister_user_test.java
+++ soft_eng_pro/src/test/java/jehad/acceptance_tests/Unregister_user_test.java
@@ -10,26 +10,34 @@ public class Unregister_user_test {
 	admin_user admin ;
 	books book ;
 	User user1;
+	ELibrary library;
 	public Unregister_user_test() {
 		 admin = new admin_user();
 		 book  =new books() ;
 		 user1= new User();
-		
+		 library = new ELibrary();
 	}	
 
 	@Given("a user is registered with the Elibrary")
 	public void aUserIsRegisteredWithTheElibrary() {
-	    
+		user1 = new User("11924313", "Jehad Nael", "jeh@gmail.com", "Sebastia street", "A26HW3", "Sebastia");
+	    library.registerUser(user1);
 	}
 
 	@Given("the user has borrowed books")
 	public void theUserHasBorrowedBooks() {
-	    
+	    book = new books();
+		user1.borrowBook(book);
 	}
 
 	@Given("the user has fines")
 	public void theUserHasFines() {
-	    
+		user1.login();
+		user1.borrowBook(book);
+		DateServer dateserver = new DateServer();
+//		DateServer dateserver = mock(DateServer.class);
+		dateserver = user1.getBookDate(book);
+		dateserver.advanceDateByDays(21);
 	}
 
 
@@ -37,11 +45,11 @@ public class Unregister_user_test {
 
 	@When("the admin try to unregister that user")
 	public void theAdminTryToUnregisterThatUser() {
-	    
+	    library.unregisterUser(user1);
 	}
 	@Then("the user is out of library")
 	public void theUserIsOutOfLibrary() {
-	    
+	    assertFalse(library.searchUser(user1));
 	}
 
 }
